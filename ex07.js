c55daf731caeb96d02452b5a766b89384ca9d549
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-03-10 22:49:18 +0100

    exo boucle for

--- ex07.js
+++ ex07.js
@@ -22,3 +22,15 @@ boostedAddition([4, 5, 6]);
 boostedAddition([4, 4, 6, 8, 10, 12]);
 
 //  écrire votre code sous ce commentaire
+
+function boostedAddition(nb) {
+    var somme = 0;
+    for(let i=0; i<nb.length; i++) {
+        somme = somme + nb[i];
+    
+}
+
+return somme;
+}
+console.log(boostedAddition([4, 5, 6]));
+console.log(boostedAddition([4, 4, 6, 8, 10, 12]));
