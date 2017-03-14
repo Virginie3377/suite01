2c9235857564574c966771428f02353b91dc7016
Virginie3377 <virginiecapdevielle77@gmail.com>
2017-03-14 15:01:10 +0100

    exo09 résolu

--- ex09.js
+++ ex09.js
@@ -22,3 +22,24 @@ boostedEvenAddition([4, 5, 6]);
 boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]);
 
 //  écrire votre code sous ce commentaire
+
+function boostedEvenAddition(tab) {
+    var nombre = 0;
+    for (let i = 0; i < tab.length; i++) {
+
+        if ((tab[i]) % 2 != 0) {
+            nombre = nombre + tab[i];
+        }
+    }
+    return boostedAddition(tab) - nombre;
+}
+
+function boostedAddition(nb) {
+    var somme = 0;
+    for (let i = 0; i < nb.length; i++) {
+        somme += nb[i];
+    }
+    return somme;
+}
+console.log(boostedEvenAddition([4, 5, 6]));
+console.log(boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]));
